e5e0c220375e5747940e851ba20cb12b29b2cb87
djiordhan <jordandiamante@icloud.com>
2023-01-26 03:23:48 +0000

    play sound

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,7 @@ import ProductMenu from './components/ProductMenu';
 import ProductSearch from './components/ProductSearch';
 import Cart from './components/Cart';
 import Checkout from './components/Checkout';
+import button from './assets/button-21.mp3';
 
 function App() {
   const initItems: any[] = [];
@@ -61,6 +62,9 @@ function App() {
                         <div className="flex-grow px-8 text-right text-lg py-4 relative">
                           <button onClick={() => {
                             setCartItems([]);
+                            const sound = new Audio();
+                            sound.src = button;
+                            sound.play();
                           }} className="text-blue-gray-300 hover:text-pink-500 focus:outline-none">
                             <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6 inline-block" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                               <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16"></path>
--- src/components/ProductItem.tsx
+++ src/components/ProductItem.tsx
@@ -1,10 +1,14 @@
 import React from 'react';
 import burger from '../assets/beef-burger.png';
+import beep from '../assets/beep-29.mp3';
 
 const ProductItem = ({ product, onSelect }: any) => {
     return <div role="button" 
             onClick={() => { 
                 onSelect(product);
+                const sound = new Audio();
+                sound.src = beep;
+                sound.play();
             }} className="select-none cursor-pointer transition-shadow overflow-hidden rounded-2xl bg-white shadow hover:shadow-lg">
             <img src={burger} alt="Beef Burger" />
             <div className="flex pb-3 px-3 text-sm -mt-3">
