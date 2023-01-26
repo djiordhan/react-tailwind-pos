a89cbb82bf366649459ee47857d48099a4b90819
djiordhan <jordandiamante@icloud.com>
2023-01-26 03:08:05 +0000

    checkout component

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import './App.css';
 import ProductMenu from './components/ProductMenu';
 import ProductSearch from './components/ProductSearch';
 import Cart from './components/Cart';
+import Checkout from './components/Checkout';
 
 function App() {
   const initItems: any[] = [];
@@ -21,7 +22,7 @@ function App() {
           <div className="flex flex-col bg-blue-gray-50 h-full w-full py-4">
             <ProductSearch onSearch={(searchKey: string) => {
               setSearchKey(searchKey);
-            }}/>
+            }} />
             <div className="h-full overflow-hidden mt-4">
               <ProductMenu searchKey={searchKey} onSelect={(product: any) => {
                 let found = false;
@@ -37,66 +38,43 @@ function App() {
                 }
 
                 setCartItems([...cartItems]);
-              }}/>
+              }} />
             </div>
           </div>
           <div className="w-5/12 flex flex-col bg-blue-gray-50 h-full bg-white pr-4 pl-2 py-4">
             <div className="bg-white rounded-3xl flex flex-col h-full shadow">
               <div className="flex-1 flex flex-col overflow-auto">
                 <div className="h-16 text-center flex justify-center">
-                  <div className="pl-8 text-left text-lg py-4 relative">
-                    <svg xmlns="http://www.w3.org/2000/svg" className="h-6 inline-block" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                      <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z"></path>
-                    </svg>
-                    <div className="text-center absolute bg-cyan-500 text-white w-5 h-5 text-xs p-0 leading-5 rounded-full -right-2 top-3">
-                      {
-                        cartItems.reduce((accumulator, item) => accumulator + item.quantity, 0)
-                      }
-                    </div>
-                  </div>
-                  <div className="flex-grow px-8 text-right text-lg py-4 relative">
-                    <button className="text-blue-gray-300 hover:text-pink-500 focus:outline-none">
-                      <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6 inline-block" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                        <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16"></path>
-                      </svg>
-                    </button>
-                  </div>
+                  {
+                    cartItems.length !== 0 &&
+                      <>
+                        <div className="pl-8 text-left text-lg py-4 relative">
+                          <svg xmlns="http://www.w3.org/2000/svg" className="h-6 inline-block" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                            <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z"></path>
+                          </svg>
+                          <div className="text-center absolute bg-cyan-500 text-white w-5 h-5 text-xs p-0 leading-5 rounded-full -right-2 top-3">
+                            {
+                              cartItems.reduce((accumulator, item) => accumulator + item.quantity, 0)
+                            }
+                          </div>
+                        </div>
+                        <div className="flex-grow px-8 text-right text-lg py-4 relative">
+                          <button onClick={() => {
+                            setCartItems([]);
+                          }} className="text-blue-gray-300 hover:text-pink-500 focus:outline-none">
+                            <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6 inline-block" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                              <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16"></path>
+                            </svg>
+                          </button>
+                        </div>
+                      </>
+                  }
                 </div>
                 <Cart items={cartItems} onCartUpdate={(newCartItems: any) => {
                   setCartItems(newCartItems);
-                }}/>
+                }} />
               </div>
-              <div className="select-none h-auto w-full text-center pt-3 pb-4 px-4">
-                <div className="flex mb-3 text-lg font-semibold text-blue-gray-700">
-                  <div>TOTAL</div>
-                  <div className="text-right w-full" x-text="priceFormat(getTotalPrice())">Rp. 16.000</div>
-                </div>
-                <div className="mb-3 text-blue-gray-700 px-3 pt-2 pb-3 rounded-lg bg-blue-gray-50">
-                  <div className="flex text-lg font-semibold">
-                    <div className="flex-grow text-left">CASH</div>
-                    <div className="flex text-right">
-                      <div className="mr-2">Rp</div>
-                      <input x-bind:value="numberFormat(cash)" type="text" className="w-28 text-right bg-white shadow rounded-lg focus:bg-white focus:shadow-lg px-2 focus:outline-none" />
-                    </div>
-                  </div>
-                  <hr className="my-2" />
-                  <div className="grid grid-cols-3 gap-2 mt-2">
-                    <button className="bg-white rounded-lg shadow hover:shadow-lg focus:outline-none inline-block px-2 py-1 text-sm">+<span>20</span></button>
-                    <button className="bg-white rounded-lg shadow hover:shadow-lg focus:outline-none inline-block px-2 py-1 text-sm">+<span>50</span></button>
-                    <button className="bg-white rounded-lg shadow hover:shadow-lg focus:outline-none inline-block px-2 py-1 text-sm">+<span>100</span></button>
-                    <button className="bg-white rounded-lg shadow hover:shadow-lg focus:outline-none inline-block px-2 py-1 text-sm">+<span>200</span></button>
-                    <button className="bg-white rounded-lg shadow hover:shadow-lg focus:outline-none inline-block px-2 py-1 text-sm">+<span>500</span></button>
-                    <button className="bg-white rounded-lg shadow hover:shadow-lg focus:outline-none inline-block px-2 py-1 text-sm">+<span>1000</span></button>
-                  </div>
-                </div>
-                <div className="flex mb-3 text-lg font-semibold bg-cyan-50 text-blue-gray-700 rounded-lg py-2 px-3">
-                    <div className="text-cyan-800">CHANGE</div>
-                    <div className="text-right flex-grow text-cyan-600">Rp. 6.222</div>
-                  </div>
-                  <button className="text-white rounded-2xl text-lg w-full py-3 focus:outline-none bg-cyan-500 hover:bg-cyan-600">
-                    SUBMIT
-                  </button>
-              </div>
+              <Checkout items={cartItems}/>
             </div>
           </div>
         </div>
---
+++ src/components/Checkout.tsx
@@ -0,0 +1,55 @@
+import React, { useState } from 'react';
+
+const Checkout = ({ items }: any) => {
+
+    const total = items.reduce((accumulator: number, item: any) => accumulator + (+item.product.price * +item.quantity), 0);
+    const [cash, setCash] = useState(0);
+    const change = cash - total;
+
+    return <div className="select-none h-auto w-full text-center pt-3 pb-4 px-4">
+        <div className="flex mb-3 text-lg font-semibold text-blue-gray-700">
+            <div>TOTAL</div>
+            <div className="text-right w-full">PHP {total}</div>
+        </div>
+        <div className="mb-3 text-blue-gray-700 px-3 pt-2 pb-3 rounded-lg bg-blue-gray-50">
+            <div className="flex text-lg font-semibold">
+                <div className="flex-grow text-left">CASH</div>
+                <div className="flex text-right">
+                    <div className="mr-2">PHP</div>
+                    <input value={cash} type="number" onChange={(event) => {
+                        setCash(+event.target.value);
+                    }} className="w-28 text-right bg-white shadow rounded-lg focus:bg-white focus:shadow-lg px-2 focus:outline-none" />
+                </div>
+            </div>
+            <hr className="my-2" />
+            <div className="grid grid-cols-3 gap-2 mt-2">
+                <button className="bg-white rounded-lg shadow hover:shadow-lg focus:outline-none inline-block px-2 py-1 text-sm" onClick={() => setCash(cash + 20)}>+<span>20</span></button>
+                <button className="bg-white rounded-lg shadow hover:shadow-lg focus:outline-none inline-block px-2 py-1 text-sm" onClick={() => setCash(cash + 50)}>+<span>50</span></button>
+                <button className="bg-white rounded-lg shadow hover:shadow-lg focus:outline-none inline-block px-2 py-1 text-sm" onClick={() => setCash(cash + 100)}>+<span>100</span></button>
+                <button className="bg-white rounded-lg shadow hover:shadow-lg focus:outline-none inline-block px-2 py-1 text-sm" onClick={() => setCash(cash + 200)}>+<span>200</span></button>
+                <button className="bg-white rounded-lg shadow hover:shadow-lg focus:outline-none inline-block px-2 py-1 text-sm" onClick={() => setCash(cash + 500)}>+<span>500</span></button>
+                <button className="bg-white rounded-lg shadow hover:shadow-lg focus:outline-none inline-block px-2 py-1 text-sm" onClick={() => setCash(cash + 1000)}>+<span>1000</span></button>
+                <button className="bg-white rounded-lg shadow hover:shadow-lg focus:outline-none inline-block px-2 py-1 text-sm" onClick={() => setCash(0)}><span>C</span></button>
+                <button className="bg-white rounded-lg shadow hover:shadow-lg focus:outline-none inline-block px-2 py-1 text-sm" onClick={() => setCash(total)}><span>Exact</span></button>
+            </div>
+        </div>
+        {
+            total > 0
+            && (
+                change >= 0 ?
+                    <div className="flex mb-3 text-lg font-semibold bg-cyan-50 text-blue-gray-700 rounded-lg py-2 px-3">
+                        <div className="text-cyan-800">CHANGE</div>
+                        <div className="text-right flex-grow text-cyan-600">PHP {change}</div>
+                    </div> :
+                    <div className="flex mb-3 text-lg font-semibold bg-pink-100 text-blue-gray-700 rounded-lg py-2 px-3">
+                        <div className="text-right flex-grow text-pink-600">PHP {change}</div>
+                    </div>
+            )
+        }
+        <button className="text-white rounded-2xl text-lg w-full py-3 focus:outline-none bg-cyan-500 hover:bg-cyan-600">
+            SUBMIT
+        </button>
+    </div>;
+};
+
+export default Checkout;
