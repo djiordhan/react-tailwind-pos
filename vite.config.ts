e673ca837530a0c336c0a8046f9e8d4344e23049
jordan <jordandiamante@icloud.com>
2024-05-22 00:59:57 +0800

    update build dir

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,14 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { resolve } from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    outDir: resolve(__dirname, '../../src/main/resources/static'),
+    emptyOutDir: true, // Clear the directory before building
+  },
   server: {
     proxy: {
       '/products': {
